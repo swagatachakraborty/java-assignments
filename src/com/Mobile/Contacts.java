8287a7194128567673f2f120d9ded80201c8174d
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-03-10 16:48:40 +0530

    Add implimentation of command line mobile app

---
+++ src/com/Mobile/Contacts.java
@@ -0,0 +1,43 @@
+package com.Mobile;
+
+import java.util.ArrayList;
+
+public class Contacts {
+    private ArrayList<Contact> contacts;
+
+    public Contacts() {
+        contacts = new ArrayList<>(10);
+    }
+
+    protected ArrayList<Contact> getContacts() {
+        return contacts;
+    }
+
+    protected void addContact(long number, String name) {
+        contacts.add(new Contact(number, name));
+    }
+
+    protected void removeContact(String name) {
+        contacts.remove(getContactByName(name));
+    }
+
+    protected Contact getContactByName(String name) {
+        for (Contact contact : contacts) {
+            if (contact.getName().equals(name)) {
+                return contact;
+            }
+        }
+        return null;
+    }
+
+    protected void updateContact(String name, String newName, long newNumber) {
+        Contact contact = this.getContactByName(name);
+        if (newName != null) {
+            contact.setName(newName);
+        }
+
+        if (newNumber != 0) {
+            contact.setPhoneNumber(newNumber);
+        }
+    }
+}
---
+++ src/com/Mobile/MobilePhone.java
@@ -0,0 +1,67 @@
+package com.Mobile;
+
+import java.util.ArrayList;
+
+public class MobilePhone {
+    private long phoneNumber;
+    private Contacts myContacts;
+
+    public MobilePhone(long phoneNumber) {
+        this.phoneNumber = phoneNumber;
+        this.myContacts = new Contacts();
+    }
+
+    protected void storeContact(long number, String name) {
+        this.myContacts.addContact(number, name);
+    }
+
+    public ArrayList<Contact> getMyContacts() {
+        return this.myContacts.getContacts();
+    }
+
+    protected void removeContact(String name) {
+        if (!this.hasContact(name)) {
+            this.showContactNotFoundError();
+            return;
+        }
+
+        myContacts.removeContact(name);
+        System.out.println("Contact with name '" + name + "' has deleted.");
+    }
+
+    private boolean hasContact(String name) {
+        return myContacts.getContactByName(name) != null;
+    }
+
+    private void showContactNotFoundError() {
+        System.out.println("There is no contact with this name.");
+    }
+
+    protected void queryContact(String name) {
+        if (this.hasContact(name)) {
+            displayContact(myContacts.getContactByName(name));
+            return;
+        }
+        this.showContactNotFoundError();
+    }
+
+    private void displayContact(Contact contact) {
+        System.out.println("Name : " + contact.getName() + ", Phone number : " + contact.getPhoneNumber());
+    }
+
+    protected void displayContacts() {
+        for (Contact contact : this.getMyContacts()) {
+            this.displayContact(contact);
+        }
+    }
+
+    protected void modifyContact(String name, String newName, long newNumber) {
+        if (!this.hasContact(name)) {
+            this.showContactNotFoundError();
+        }
+
+        myContacts.updateContact(name, newName, newNumber);
+        System.out.println("Contact with the name " + name + " has been modified.");
+        return;
+    }
+}
